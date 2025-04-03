98975111c1ce88d8dc24d56a92cb410f71762b8f
tailor-releaser <tailor-releaser@users.noreply.github.com>
2025-04-03 05:22:07 +0000

    Brew formula update for tailorctl version v1.29.1

--- Formula/tailorctl.rb
+++ Formula/tailorctl.rb
@@ -4,68 +4,48 @@
 class Tailorctl < Formula
   desc "Command line tool for Tailor Platform'"
   homepage "https://tailor.tech"
-  version "1.29.0"
+  version "1.29.1"
 
   depends_on "cue" => :optional
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tailor-platform/tailorctl/releases/download/v1.29.0/tailorctl_darwin_v1.29.0_arm64.tar.gz"
-      sha256 "b8857d4f32530f9dc235cbce2c56cc473ad375bbb9efb279b2e086578fe1cf8d"
+      url "https://github.com/tailor-platform/tailorctl/releases/download/v1.29.1/tailorctl_darwin_v1.29.1_arm64.tar.gz"
+      sha256 "01e55dca92256e292b3aa9cdaa7f97c1f3ece947cdafdb2942fd8cc62573cfa3"
 
       def install
         bin.install "tailorctl"
-        bash_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "bash")
-        (bash_completion/"tailorctl").write bash_output
-        zsh_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "zsh")
-        (zsh_completion/"tailorctl").write zsh_output
-        fish_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "fish")
-        (fish_completion/"tailorctl.fish").write fish_output
+        generate_completions_from_executable(bin/"tailorctl", "completion")
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/tailor-platform/tailorctl/releases/download/v1.29.0/tailorctl_darwin_v1.29.0_x86_64.tar.gz"
-      sha256 "11877f7fcbb13d73770d24dbf537c11e3e680940821eddafa2b7137a7af4f17e"
+      url "https://github.com/tailor-platform/tailorctl/releases/download/v1.29.1/tailorctl_darwin_v1.29.1_x86_64.tar.gz"
+      sha256 "a2a0250b809ebd0bd0cc5a162dbc3b7e4d08f9345426952105e3ed4f7ff05d93"
 
       def install
         bin.install "tailorctl"
-        bash_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "bash")
-        (bash_completion/"tailorctl").write bash_output
-        zsh_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "zsh")
-        (zsh_completion/"tailorctl").write zsh_output
-        fish_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "fish")
-        (fish_completion/"tailorctl.fish").write fish_output
+        generate_completions_from_executable(bin/"tailorctl", "completion")
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/tailor-platform/tailorctl/releases/download/v1.29.0/tailorctl_linux_v1.29.0_arm64.tar.gz"
-      sha256 "75bd4afc68dd7b3bceb0c4f37e8365c0ecbd306e934d62a7e508cb7f5b3f89b9"
+      url "https://github.com/tailor-platform/tailorctl/releases/download/v1.29.1/tailorctl_linux_v1.29.1_arm64.tar.gz"
+      sha256 "1d24ef161e21b39db68d0f761f61e3b339d4cefa0233ef53e3feb35c828e1ed5"
 
       def install
         bin.install "tailorctl"
-        bash_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "bash")
-        (bash_completion/"tailorctl").write bash_output
-        zsh_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "zsh")
-        (zsh_completion/"tailorctl").write zsh_output
-        fish_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "fish")
-        (fish_completion/"tailorctl.fish").write fish_output
+        generate_completions_from_executable(bin/"tailorctl", "completion")
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/tailor-platform/tailorctl/releases/download/v1.29.0/tailorctl_linux_v1.29.0_x86_64.tar.gz"
-      sha256 "954d2bf8151e9662e96385ba48b4fa1d5f84b7b194d1c4c80872f94da0fa1bb7"
+      url "https://github.com/tailor-platform/tailorctl/releases/download/v1.29.1/tailorctl_linux_v1.29.1_x86_64.tar.gz"
+      sha256 "c3e182c55fd643e965b62a36b0fd62687b3944754d7cb676aa6b98f474f337c7"
 
       def install
         bin.install "tailorctl"
-        bash_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "bash")
-        (bash_completion/"tailorctl").write bash_output
-        zsh_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "zsh")
-        (zsh_completion/"tailorctl").write zsh_output
-        fish_output = Utils.safe_popen_read(bin/"tailorctl", "completion", "fish")
-        (fish_completion/"tailorctl.fish").write fish_output
+        generate_completions_from_executable(bin/"tailorctl", "completion")
       end
     end
   end
