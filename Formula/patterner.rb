66f09367e5b20265476bfe7daebefbd6da21885e
k1LoW <k1LoW@users.noreply.github.com>
2025-09-11 18:02:41 +0000

    Commit from GitHub Actions (Update formulae)

--- Formula/patterner.rb
+++ Formula/patterner.rb
@@ -1,27 +1,27 @@
 class Patterner < Formula
   desc 'patterner is a tool to analyze and present best practices (patterns) for Tailor Platform applications.'
-  version '0.4.2'
+  version '0.5.0'
   homepage 'https://github.com/tailor-platform/patterner'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/tailor-platform/patterner/releases/download/v0.4.2/patterner_v0.4.2_darwin_arm64.zip'
-      sha256 '4d182ae9fcf81a8183c344aabb3de4d48901d78c788f1e05e4d9706050380fe8'
+      url 'https://github.com/tailor-platform/patterner/releases/download/v0.5.0/patterner_v0.5.0_darwin_arm64.zip'
+      sha256 'db884a78cc675f5b441282c8763385fcf723c2f84bdf5ca10c2c12a3398b4bba'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/tailor-platform/patterner/releases/download/v0.4.2/patterner_v0.4.2_darwin_amd64.zip'
-      sha256 'f608b95861e81ee742fd30a770d5167d57b6b58e6a2b4f9a35147680314ac9f1'
+      url 'https://github.com/tailor-platform/patterner/releases/download/v0.5.0/patterner_v0.5.0_darwin_amd64.zip'
+      sha256 '4ca3b273f28a6ba2b8cecb92d3b38241764a6553a149583dab78c36df9f61224'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/tailor-platform/patterner/releases/download/v0.4.2/patterner_v0.4.2_linux_arm64.tar.gz'
-      sha256 'd8e2941c1edb918a2262fe22a91cd52718baf19c4d6300a89dc04087208aa405'
+      url 'https://github.com/tailor-platform/patterner/releases/download/v0.5.0/patterner_v0.5.0_linux_arm64.tar.gz'
+      sha256 '4a2176bf34e337ec2a9937f330de6a1e963ec3ff81f7dbef62f2cbf76df164b6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/tailor-platform/patterner/releases/download/v0.4.2/patterner_v0.4.2_linux_amd64.tar.gz'
-      sha256 '289492230db4068461b02b0093a77983c6644bc7f820f8fc051f21b68fb3bb32'
+      url 'https://github.com/tailor-platform/patterner/releases/download/v0.5.0/patterner_v0.5.0_linux_amd64.tar.gz'
+      sha256 '8d865562bf826361c049525a7276426e68cf3a2871424d6d34c4016c90ee826b'
     end
   end
 
